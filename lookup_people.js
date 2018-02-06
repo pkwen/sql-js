685a4aa635b0786135b34d8ea4948aba1c23f864
Danny K Pan <kaiwen006@gmail.com>
2018-02-06 16:32:01 +0000

    added query by name file that searches famous_people table

---
+++ lookup_people.js
@@ -0,0 +1,24 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+let name = process.argv[2];
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * from famous_people where first_name = $1 or last_name = $1", [name], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows);
+    client.end();
+  });
+});
